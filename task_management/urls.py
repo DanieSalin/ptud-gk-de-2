825b4fed5ef157aeb31d74b3e6dd98e7658fee1c
Your Name <your.email@example.com>
2025-03-14 20:13:12 +0700

    thay doi

---
+++ task_management/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+from users.views import logout_view
+from users.views import update_avatar
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('tasks.urls')),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # Cập nhật đường dẫn logout để chuyển hướng trực tiếp đến trang login
+    path('logout/', logout_view, name='logout'),
+    path('update-avatar/', update_avatar, name='update_avatar'),
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
